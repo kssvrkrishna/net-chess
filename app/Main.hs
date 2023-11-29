729858a58f73bfdc38412f0d38b4e35a236a18bc
bavanya kurra <bavanya@bavanyas-Air.lan1>
2023-11-28 23:36:25 -0800

    Add dependency on Brick and Network

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
 module Main where
-
 import qualified MyLib (someFunc)
+import HaskellSay (haskellSay)
+import Brick
+import Network.Socket
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
+  haskellSay "Hello, Haskell! You're using a function from another package!"
   MyLib.someFunc
